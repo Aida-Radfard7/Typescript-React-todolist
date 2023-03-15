4d4bd565b92e294736a59fb4c79b07a1b29cf4ae
aida radfard <aida.radfard.1@gmail.com>
2023-03-15 22:32:45 +0330

    handle add todo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React , { useState, useRef } from 'react';
 import './App.scss';
 
 function App() {
+
+  type todoType = {
+    title : string,
+    id : string
+  }
+
+  const [items , setItems] = useState<todoType[]>([])
+  const input  = useRef<HTMLInputElement>(null);
+
+  const addNewToDo = (newToDo : string | undefined) : void =>{
+    if (newToDo == undefined) return
+    const newItem = {title : newToDo , id : '1'}
+    setItems([...items , newItem])
+  }
+
+
   return (
     <div className="App">
       <form id="form">
+
         <section className='form__header'>
           <label className='input'>
-            <input className='input__field' type="text" placeholder=" " />
+            <input 
+              className='input__field' 
+              type="text"
+              ref={input}  />
             <span className='input__label'>add your to do ...</span>
           </label>
-          <button className='btn-submit'>Add</button>
+          <button onClick={() => addNewToDo(input.current?.value)} className='btn-submit' type='button'>Add</button>
         </section>
+
+        <section className='list'>
+          <ul className='list__items'>
+            {items.map(item => 
+              <li className='list__item'>{item.title}</li>
+            )}
+          </ul>
+        </section>
+
     </form>
     </div>
   );
